486b7ae0b639a6f306c7ce4cec23d498949ef21c
lemonadedance <sopphey@gmail.com>
2022-11-03 20:49:15 -0500

    setup for E2E tests

---
+++ src/test/java/com/revature/steps/ResetPasswordSteps.java
@@ -0,0 +1,10 @@
+package com.revature.steps;
+
+import com.revature.runner.Runner;
+import org.openqa.selenium.WebDriver;
+
+public class ResetPasswordSteps {
+
+    public static WebDriver driver = Runner.driver;
+
+}
---
+++ src/test/java/com/revature/steps/UserProfileSteps.java
@@ -0,0 +1,10 @@
+package com.revature.steps;
+
+import com.revature.runner.Runner;
+import org.openqa.selenium.WebDriver;
+
+public class UserProfileSteps {
+
+    public static WebDriver driver = Runner.driver;
+
+}
