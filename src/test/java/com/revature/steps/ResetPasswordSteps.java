3a7532187c8cefb85ef9fe8ec6e1585f2bc10190
lemonadedance <sopphey@gmail.com>
2022-11-04 10:24:40 -0500

    adding pre merge from main to VBranch changes

--- src/test/java/com/revature/steps/ResetPasswordSteps.java
+++ src/test/java/com/revature/steps/ResetPasswordSteps.java
@@ -1,10 +1,31 @@
 package com.revature.steps;
 
 import com.revature.runner.Runner;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 
 public class ResetPasswordSteps {
 
     public static WebDriver driver = Runner.driver;
+    @Given("the user is on the reset password page")
+    public void the_user_is_on_the_reset_password_page() {
+        //
+    }
 
+    @When("the user types in their email address")
+    public void the_user_types_in_their_email_address() {
+        //
+    }
+
+    @When("the user clicks on reset password link button")
+    public void the_user_clicks_on_reset_password_link_button() {
+        //
+    }
+
+    @Then("the user should receive a confirmation alert that they reset their password")
+    public void the_user_should_receive_a_confirmation_alert_that_they_reset_their_password() {
+        //
+    }
 }
